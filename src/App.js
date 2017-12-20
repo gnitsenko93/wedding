d1614f23d29bff654e84648e034d3dfa7f07bab0
Gregory.Nitsenko <gregory.nitsenko@nordigy.ru>
2017-12-20 21:04:33 +0300

    further styling

--- src/App.js
+++ src/App.js
@@ -16,22 +16,27 @@ class App extends Component {
 	render() {
     		return (
                 <div className="app">
-                    <div className="app__section">
-                        <Summary summary={this.state.summary}/>
+                    <div>
+                        <div className="app__section">
+                            <Summary summary={this.state.summary}/>
+                        </div>
+                        <div className="app__section">
+                            <Overview description={this.state.description}
+                                      dates={this.state.dates}
+                                      time={this.state.time}/>
+                        </div>
+                        <div className="app__section">
+                            {
+                                this.state.places.map(place => <Place key={place.id}
+                                                                      name={place.name}
+                                                                      purpose={place.purpose}
+                                                                      time={place.time}
+                                                                      address={place.address}/>)
+                            }
+                        </div>
                     </div>
-                    <div className="app__section">
-                        <Overview description={this.state.description}
-                                  dates={this.state.dates}
-                                  time={this.state.time}/>
-                    </div>
-                    <div className="app__section">
-                        {
-                            this.state.places.map(place => <Place key={place.id}
-                                                                  name={place.name}
-                                                                  purpose={place.purpose}
-                                                                  time={place.time}
-                                                                  address={place.address}/>)
-                        }
+                    <div>
+
                     </div>
                 </div>
     		);
