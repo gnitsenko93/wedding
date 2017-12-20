48dbc921727df4ddc233f54266ac49aecaa8950c
Greg Nitsenko <gregory.nitsenko@nordigy.ru>
2017-12-20 16:44:04 +0300

    adding href to map for a place

--- src/components/Place.js
+++ src/components/Place.js
@@ -4,10 +4,14 @@ import '../styles/Place.css';
 class Place extends Component {
 	render() {
 	    return <div className="place">
-            <span>{this.props.purpose}</span>
-            <span>{this.props.name}</span>
+            <span className="place__purpose">{this.props.purpose}</span>
+            {
+                this.props.address?
+                    <a className="place__name place__address" href={this.props.address} target="_blank">{this.props.name}</a>
+                    :
+                    <span className="place__name">{this.props.name}</span>
+            }
             <span className="timestamp">{this.props.time}</span>
-            <span>{this.props.address}</span>
         </div>;
     }
 }
